4617f5ba10e16b50515ed2c5c6dc836c665e1a99
Asmodin225 <student5513@liceum15.ukr.education>
2025-04-24 08:47:20 +0300

    test lui

---
+++ test lui.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+win = Tk()
+win.title('My project')
+win.geometry('500x300')
+win.resizable(False,False) # заборона зміни розміру вікна
+#  елементи керування
+# написи
+nap = Label(win,text='текст для напису', font=('Comic Sans №5') )
+
+
+win.mainloop()
